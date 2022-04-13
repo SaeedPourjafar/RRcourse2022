7981cd12104ac14a18886960d884a42bd4d47c5b
Saeed Pourjafar <41834810+SaeedPourjafar@users.noreply.github.com>
2022-04-13 21:14:57 +0200

    Assignment_3_DONE created
    Assignment_3_DONE has created and Codes and instruction.R has modified.
    The results of Codes and instruction.R are 8 reports in pdf.

--- RR_Apr_07_13/Codes and instructions.R
+++ RR_Apr_07_13/Codes and instructions.R
@@ -2,7 +2,7 @@
 # Reproducible Research 2022
 # Wojciech Hardy & Lukasz Nawaro
 
-setwd("")
+setwd("C:/Users/Saeed/Documents/GitHub/RRcourse2022/RR_Apr_07_13")
 
 # We need to load the necessary packages if we're operating through an R script.
 
@@ -10,13 +10,23 @@ library(rmarkdown)
 
 # We can render an .Rmd document by using the render() function
 
-render("RMD_class_1_cut.Rmd")
+# render("RMD_class_1_cut.Rmd")
+
+# Running the code below will create 8 reports in pdf
+for (data in c('season_1.RDATA','season_2.RDATA',
+               'season_3.RDATA','season_4.RDATA',
+               'season_5.RDATA','season_6.RDATA',
+               'season_7.RDATA','season_8.RDATA')) {
+  rmarkdown::render("Assignment_3_DONE.Rmd", 
+                    output_file = paste0('Season ',substr(data,8,8),'_report','.pdf'),
+                    params = list(data = data))
+}
 
 # The render function can overwrite the document settings
 
-render("RMD_class_1_cut.Rmd", output_format = "word_document")   # switches the output format
+# render("RMD_class_1_cut.Rmd", output_format = "pdf_document")   # switches the output format
 
-render("RMD_class_1_cut.Rmd", clean = FALSE)   # tells knitr to keep the intermediate files (e.g. ".md")
+# render("RMD_class_1_cut.Rmd", clean = FALSE)   # tells knitr to keep the intermediate files (e.g. ".md")
 
 # See https://www.rdocumentation.org/packages/rmarkdown/versions/2.6/topics/render for more
 
@@ -31,13 +41,13 @@ render("RMD_class_1_cut.Rmd", clean = FALSE)   # tells knitr to keep the interme
 # Main goal: modify the Assignment 3 content to make it an automated report
 # that can be used for any of the Game of Thrones seasons.
 # 
-# - Use params. 
+# - Use params. (DONE, the param name is 'data')
 # 
 # - You can refer to the files in the Data folder as input or you can visit
-# the Wikipedia pages for specific seasons and use web scraping (the longer but possible road).
+# the Wikipedia pages for specific seasons and use web scraping (the longer but possible road). (DONE)
 # 
 # - Change the relevant numbers, names, descriptions, etc. in the text so that
-# the report includes info for the correct season
+# the report includes info for the correct season (DONE)
 # 
-# - If you get that far, do a loop with render() to quickly create 8 reports in pdfs.
+# - If you get that far, do a loop with render() to quickly create 8 reports in pdfs. (DONE)
 
